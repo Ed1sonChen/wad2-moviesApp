2ee0c501b7ab7ce944d2ead8760868e5e0fb0229
Chen Zhiling <20091576@mail.wit.ie>
2020-11-08 14:08:27 +0800

    New button for upcoming movies page

---
+++ src/components/buttons/addToWatchList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AddToWatchListButton = () => {
+  return (
+    <button
+      type="button"
+      className="btn w-100 btn-primary"
+    >
+      Add to watch list
+    </button>
+  );
+};
+
+export default AddToWatchListButton;
--- src/pages/upComingMoviesPage.js
+++ src/pages/upComingMoviesPage.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import {upcomingMoviesContext} from '../contexts/upcomingMoviesContext'
-import AddToFavoritesButton from '../components/buttons/addToFavorites'
+import AddToWatchListButton from '../components/buttons/addToWatchList'
 
 const MovieListPage = () => {
   const context = useContext(upcomingMoviesContext)
@@ -11,7 +11,7 @@ const MovieListPage = () => {
       title="Upcoming Movies"
       movies={context.movies}  /* Changed */
       action={(movie) => {
-        return <AddToFavoritesButton movie={movie} />;
+        return <AddToWatchListButton movie={movie} />;
       }}
     />
   );
