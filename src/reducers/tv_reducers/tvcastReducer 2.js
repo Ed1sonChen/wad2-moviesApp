b668081e0fff560a078bcdbe0b2660870f2e14b7
Chen Zhiling <20091576@mail.wit.ie>
2020-12-10 19:47:56 +0800

    Fix and add lint to yml

---
+++ src/reducers/tv_reducers/tvcastReducer 2.js
@@ -0,0 +1,16 @@
+import { GET_TV_CASTS } from "../../actions/types";
+
+const initialState = {
+    tvcasts: []
+};
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case GET_TV_CASTS:
+            return {
+                ...state,
+                tvcasts: action.payload
+            };
+        default:
+            return state;
+    }
+};
